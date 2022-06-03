80a97fbc883bcc682fa98b08a7dee3128e7db678
Ayush2122 <106580366+Ayush2122@users.noreply.github.com>
2022-06-03 12:17:31 +0545

    first commit

---
+++ responsive website/new.js
@@ -0,0 +1,12 @@
+function my2(){
+    window.open("javascript.pdf");
+}
+function html(){
+    window.open("html.pdf");
+}
+function css(){
+    window.open("css.pdf");
+}
+function boot(){
+    window.open("bootstrap.pdf");
+}
